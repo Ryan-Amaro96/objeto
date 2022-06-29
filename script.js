9b7343dc579fa05023766b79e5b4848e754b4728
Ryan Amaro <ryanamaro2003.st@gmail.com>
2022-06-29 02:04:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let dados={
+    time:"Flamengo",
+    corFavorita:"Vermelho",
+    joogoFavorito: "fifa"
+}
+
+dados.estiloMusical ="rap/trap"
+delete dados.joogoFavorito
+
+
+let cadastro = [{
+    nome:"Gabriel", 
+    idade:25,
+    telefone:"(21)xxxx-xxxx",
+    amigos:["Pedro","Bruno","Mario","Victor"] ,
+  },
+  
+  {
+    nome:"Everton", 
+    idade:33,
+    telefone:"(21)xxxx-xxxx",
+amigos:["Diego","Lázaro","Matheus","Thiago"] ,
+  },
+  
+  {
+    nome:"João", 
+    idade:21,
+    telefone:"(21)xxxx-xxxx",
+    amigos:["Willian","Filipe","Ayrton","Léo"] ,
+  }, 
+  {
+    nome:"Mauricio", 
+    idade:34,
+    telefone:"(21)xxxx-xxxx",
+    amigos:["Marcelo","Pablo","Fabricio","Cleiton"] ,
+  },
+  {
+    nome:"David", 
+    idade:35,
+    telefone:"(21)xxxx-xxxx",
+    amigos:["Gustavo","Rodrigo","Hugo","Marcelo"],
+  }, 
+  
+  ]
+  
+  console.log(cadastro[0].amigos[1])
+  console.log(cadastro[1].amigos[1])
+  console.log(cadastro[2].amigos[1])
+  console.log(cadastro[3].amigos[1])
+  console.log(cadastro[4].amigos[1])
